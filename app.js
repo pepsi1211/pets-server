b1fe64041af8509ca09fad21bcd2771039f9d4fc
洪智豪 <pepsi1211@163.com>
2022-04-19 15:13:55 +0800

    修改不验证权限路由

--- app.js
+++ app.js
@@ -1,104 +1,107 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var session = require('express-session');
-var UserModel = require('./models/users');
-var AdminModel = require('./models/admin');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var session = require("express-session");
+var UserModel = require("./models/users");
+var AdminModel = require("./models/admin");
+let { parseTime } = require("./utils/base");
 
-var { Mongoose, JWT } = require('./utils/config')
+var { Mongoose, JWT } = require("./utils/config");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin');
-var uploadRouter = require('./routes/upload');
-var categoryRouter = require('./routes/category');
-var productRouter = require('./routes/product');
-var fosterCareClientRouter = require('./routes/fosterCareClient');
-var fosterCareServerRouter = require('./routes/fosterCareServer');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var adminRouter = require("./routes/admin");
+var uploadRouter = require("./routes/upload");
+var categoryRouter = require("./routes/category");
+var productRouter = require("./routes/product");
+var fosterCareClientRouter = require("./routes/fosterCareClient");
+var fosterCareServerRouter = require("./routes/fosterCareServer");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(session({
-  secret: 'keyboard cat',
-  name: "sessionId",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24
-  }
-}))
-
+app.use(
+  session({
+    secret: "keyboard cat",
+    name: "sessionId",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.all('/*',(req, res, next)=>{
-  console.log('触发了app.all, 通过的url为:',req.url);
+app.all("/*", (req, res, next) => {
+  console.log("触发了app.all, 通过的url为:", req.url, parseTime(new Date()));
   // 普通路由 无需验证权限
   let constantRouter = [
-    '/api/users/register',
-    '/api/users/login',
-    '/api/users/verifyImg',
-    '/api/admin/getToken',
-    '/api/admin/register'
+    "/api/users/register",
+    "/api/users/login",
+    "/api/users/verifyImg",
+    "/api/admin/getToken",
+    "/api/admin/register",
+    "/api/client/addFosterCare",
+    "/api/client/queryFosterCare",
   ];
-  if(constantRouter.includes(req.url)){
+  if (constantRouter.includes(req.url)) {
     // 如果是注册或者登陆 直接通过,不验证token
     next();
-  }else{
+  } else {
     var token = req.headers.authorization;
     var decoded = JWT.verifyToken(token);
-    if(!decoded.username){
+    if (!decoded.username) {
       res.send({
         status: 404,
-        msg: 'token验证不通过'
-      })
+        msg: "token验证不通过",
+      });
       return;
     }
     const { username } = decoded;
     var result;
-    if(req.url.split('/')[2] == 'admin'){
-      result = AdminModel.getToken({username})
-    }else{
-      result = UserModel.findLogin({username})
+    if (req.url.split("/")[2] == "admin") {
+      result = AdminModel.getToken({ username });
+    } else {
+      result = UserModel.findLogin({ username });
     }
-    result.select('username').exec(function(err,res){
-      if(err){
-        return false
-      }else{
+    result.select("username").exec(function (err, res) {
+      if (err) {
+        return false;
+      } else {
         // verifyRes = res;
-        if(res.username == username){
+        if (res.username == username) {
           next();
-        }else{
+        } else {
           res.send({
             status: 404,
-            msg: '无法验证token,可能已过期'
-          })
+            msg: "无法验证token,可能已过期",
+          });
         }
       }
-    })
+    });
   }
-})
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/admin', adminRouter);
-app.use('/api/admin', categoryRouter);
-app.use('/api/admin', uploadRouter);
-app.use('/api/admin', productRouter);
-app.use('/api/client', fosterCareClientRouter);
-app.use('/api/admin', fosterCareServerRouter);
-
+});
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/admin", categoryRouter);
+app.use("/api/admin", uploadRouter);
+app.use("/api/admin", productRouter);
+app.use("/api/client", fosterCareClientRouter);
+app.use("/api/admin", fosterCareServerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -111,11 +114,11 @@ Mongoose.connect();
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
